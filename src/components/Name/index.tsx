a6acb9f23f1bd7d421fde563fe4e50ed9d72c72b
Phạm Văn Hoàng <vanhoangit2001@gmail.com>
2025-04-21 13:13:44 +0700

    update: input

--- src/components/Name/index.tsx
+++ src/components/Name/index.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, forwardRef, useCallback, useEffect } from 'react';
+import { SetStateAction, useCallback, useEffect } from 'react';
 import Input from '../Input/Input';
 
 interface InputProps {
@@ -9,55 +9,55 @@ interface InputProps {
   run: boolean;
 }
 
-export const Name: React.FC<React.HTMLProps<HTMLInputElement> & InputProps> =
-  forwardRef(
-    (
-      { name, setName, shareMode, playing, run }: InputProps,
-      ref: React.LegacyRef<HTMLInputElement>
-    ) => {
-      console.log('🚀 ~ playing:', playing);
-      console.log('🚀 ~ shareMode:', shareMode);
-      const onChange = useCallback(
-        (e: { target: { value: SetStateAction<string> } }) => {
-          setName(e.target.value);
-          window.history.pushState({}, '', `?name=${e.target.value}`);
-        },
-        [setName]
-      );
-
-      useEffect(() => {
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        const nameParam = urlParams.get('name');
-        if (nameParam !== null) {
-          setName(nameParam);
-        }
-      }, [setName]);
-
-      return (
-        <div
-          style={{
-            position: 'absolute',
-            top: '25%',
-            left: '50%',
-            transform: 'translateX(-50%)',
-            display: (shareMode || playing) && name.length === 0 ? 'none' : 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '100dvw',
-            zIndex: 40,
-          }}
-        >
-          <Input
-            value={name}
-            onChange={onChange}
-            disabled={shareMode || playing || run}
-            readOnly={shareMode || playing || run}
-            autoFocus={true}
-            shareMode={shareMode}
-            playing={playing}
-          />
-        </div>
-      );
-    }
+export const Name: React.FC<React.HTMLProps<HTMLInputElement> & InputProps> = ({
+  name,
+  setName,
+  shareMode,
+  playing,
+  run,
+}) => {
+  console.log('🚀 ~ playing:', playing);
+  console.log('🚀 ~ shareMode:', shareMode);
+  const onChange = useCallback(
+    (e: { target: { value: SetStateAction<string> } }) => {
+      setName(e.target.value);
+      window.history.pushState({}, '', `?name=${e.target.value}`);
+    },
+    [setName]
   );
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const nameParam = urlParams.get('name');
+    if (nameParam !== null) {
+      setName(nameParam);
+    }
+  }, [setName]);
+
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: '25%',
+        left: '50%',
+        transform: 'translateX(-50%)',
+        display: (shareMode || playing) && name.length === 0 ? 'none' : 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100dvw',
+        zIndex: 40,
+      }}
+    >
+      <Input
+        value={name}
+        onChange={onChange}
+        disabled={shareMode || playing || run}
+        readOnly={shareMode || playing || run}
+        autoFocus={true}
+        shareMode={shareMode}
+        playing={playing}
+      />
+    </div>
+  );
+};
